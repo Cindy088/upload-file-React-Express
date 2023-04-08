6d56c8c33905db2b4733f9f363a416674f163196
Cindy088 <elisabeth08@163.com>
2023-04-08 14:50:16 +1000

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
+const path = require("path");
+
+//create web server
+const app = express();
+
+app.use(cors());
+app.use(fileUpload());
+
+//error middleware
+app.use((err, req, res, next) => {
+  res.status(500).send("Server Error");
+});
+
+app.post("/upload", (req, res) => {
+  //check if file was uploaded
+  if (!req.files || !req.files.file) {
+    return res.status(400).json({
+      error: "No file was uploaded",
+    });
+  }
+
+  const file = req.files.file;
+  //validate file size (and type)
+  const maxSize = 10 * 1024 * 1024; //10mb
+  if (file.size > maxSize) {
+    return res.status(400).json({ error: "File size is too large" });
+  }
+  //generate unique filename
+  const fileName = uuidv4() + path.extname(file.name);
+  const upload_dir = `${__dirname}/client/public/uploads`; //path
+  //save file to uploads folder
+  file.mv(`${upload_dir}/${fileName}`, (err) => {
+    //move file to uploads folder
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json({ fileName: fileName, filePath: `/uploads/${fileName}` });
+  });
+});
+
+app.listen(80, () => console.log("server is running on http://localhost:80"));
